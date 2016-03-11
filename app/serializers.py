f84e08befb612ec20781417b5f60127d1d0976a7
taapesh <starmansix@gmail.com>
2016-03-10 18:52:06 -0600

    Fixed typo in serializers

--- app/serializers.py
+++ app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import TablemateUser, Table
+from models import TablemateUser, Table, ServerRegistration
 
 class TableSerializer(serializers.ModelSerializer):
 
